bf2b6cb58998f4bfded88fe6ed1c4c13fc9ecc53
Alex-Mathai-98 <alexmathai98@gmail.com>
2020-02-09 15:16:12 +0530

    grid features

---
+++ Image_Features/grid_features.py
@@ -0,0 +1,24 @@
+from torchvision import models
+import torch
+from torch import nn
+
+class GridFeatures(nn.Module) :
+
+	def __init__(self,code=None):
+
+		super(GridFeatures,self).__init__()
+
+		if code == "resnet101" :
+			resnet = models.resnet101(pretrained=True)
+			self.model = torch.nn.Sequential( *list(resnet.children())[:-2] )
+
+	def forward(self,X):
+		return self.model(X)
+
+
+
+if __name__ == '__main__' :
+
+	X = torch.randn((1,3,448,448))
+	alex = GridFeatures("resnet101")
+	alex(X)
