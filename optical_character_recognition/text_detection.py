1299e55630431b7b9c2fc8db4d478fd574567172
amanshenoy <amn.shenoy@gmail.com>
2020-03-25 20:20:40 +0530

    Text recognition added, will clean code

--- optical_character_recognition/text_detection.py
+++ optical_character_recognition/text_detection.py
@@ -21,95 +21,105 @@ def copyStateDict(state_dict):
 def str2bool(v):
     return v.lower() in ("yes", "y", "true", "t", "1")
 
-def text_detector(net, image, text_threshold = 0.7, link_threshold = 0.4, low_text = 0.4, cuda = torch.cuda.is_available(), poly = False, canvas_size = 1280, mag_ratio = 1.5, refine_net=None):
-    img_resized, target_ratio, size_heatmap = utils.resize_aspect_ratio(image, canvas_size, interpolation=cv2.INTER_LINEAR, mag_ratio=mag_ratio)
-    ratio_h = ratio_w = 1 / target_ratio
-    x = utils.normalizeMeanVariance(img_resized)
-    x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
-    x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
-    if cuda:
-        x = x.cuda()
+class TextDetection(nn.Module):
+    def __init__(self, trained_model, refine = False, model = 'CRAFT', refiner_model = None, poly = False):
+        super().__init__()
+        self.refine = refine
+        self.refiner_model = refiner_model
+        self.poly = poly
+        self.trained_model = trained_model
+        if model == 'CRAFT':
+            self.model = CRAFT()  
+        
+        if torch.cuda.is_available():
+            self.model.load_state_dict(copyStateDict(torch.load(self.trained_model)))
+            self.model = self.model.cuda()
+            self.model = torch.nn.DataParallel(self.model)
+            cudnn.benchmark = False
+        else:
+            self.model.load_state_dict(copyStateDict(torch.load(self.trained_model, map_location='cpu')))
+        self.model.eval()
+        
+        if self.refine:
+            from refinenet import RefineNet
+            refine_net = RefineNet()
+            print('Loading weights of refiner from checkpoint (' + self.refiner_model + ')')
+            if torch.cuda.is_available():
+                refine_net.load_state_dict(copyStateDict(torch.load(self.refiner_model)))
+                refine_net = refine_net.cuda()
+                refine_net = torch.nn.DataParallel(refine_net)
+            else:
+                refine_net.load_state_dict(copyStateDict(torch.load(self.refiner_model, map_location='cpu')))
+            refine_net.eval()
+            self.poly = True
 
-    # forward pass
-    with torch.no_grad():
-        y, feature = net(x)
+    def forward(self, image_path, text_threshold = 0.7, link_threshold = 0.4, low_text = 0.4, cuda = torch.cuda.is_available(), poly = False, canvas_size = 1280, mag_ratio = 1.5, refine_net=None):
+        image = self._load_image(image_path)
+        img_resized, target_ratio, size_heatmap = utils.resize_aspect_ratio(image, canvas_size, interpolation=cv2.INTER_LINEAR, mag_ratio=mag_ratio)
+        ratio_h = ratio_w = 1 / target_ratio
+        x = utils.normalizeMeanVariance(img_resized)
+        x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
+        x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
+        if cuda:
+            x = x.cuda()
 
-    # make score and link map
-    score_text = y[0,:,:,0].cpu().data.numpy()
-    score_link = y[0,:,:,1].cpu().data.numpy()
-
-    # refine link
-    if refine_net is not None:
+        # forward pass
         with torch.no_grad():
-            y_refiner = refine_net(y, feature)
-        score_link = y_refiner[0,:,:,0].cpu().data.numpy()
+            y, feature = self.model(x)
 
-    # Post-processing
-    boxes, polys = utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+        # make score and link map
+        score_text = y[0,:,:,0].cpu().data.numpy()
+        score_link = y[0,:,:,1].cpu().data.numpy()
 
-    # coordinate adjustment
-    boxes = utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
-    polys = utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
-    for k in range(len(polys)):
-        if polys[k] is None: polys[k] = boxes[k]
+        # refine link
+        if refine_net is not None:
+            with torch.no_grad():
+                y_refiner = refine_net(y, feature)
+            score_link = y_refiner[0,:,:,0].cpu().data.numpy()
 
-    # render results (optional)
-    render_img = score_text.copy()
-    ret_score_text = utils.cvt2HeatmapImg(render_img)
-    return boxes, polys, ret_score_text
+        # Post-processing
+        boxes, polys = utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+
+        # coordinate adjustment
+        boxes = utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
+        polys = utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
+        for k in range(len(polys)):
+            if polys[k] is None: polys[k] = boxes[k]
+
+        # render results (optional)
+        render_img = score_text.copy()
+        ret_score_text = utils.cvt2HeatmapImg(render_img)
+        return boxes, polys, ret_score_text
+
+    def _load_image(self, image_path):
+        return utils.loadImage(image_path)
+
+    def display(self, image_path, result_folder):
+        image = self._load_image(image_path)
+        _, polys, _ = self.forward(image_path)
+        utils.display(image_path, image[:,:,::-1], polys, dirname=result_folder)
+
+    def write_heatmap(self, image_path, result_folder):
+        filename, file_ext = os.path.splitext(os.path.basename(image_path))
+        mask_file = result_folder + "/letter_heatmap_" + filename + '.jpg'
+        _, _, score_text = self.forward(image_path)
+        cv2.imwrite(mask_file, score_text)        
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='CRAFT Text Detection')
     parser.add_argument('--trained_model', default='optical_character_recognition/pretrained_models/craft_mlt_25k.pth', type=str, help='pretrained model')
-    parser.add_argument('--text_threshold', default=0.7, type=float, help='text confidence threshold')
-    parser.add_argument('--low_text', default=0.4, type=float, help='text low-bound score')
-    parser.add_argument('--link_threshold', default=0.4, type=float, help='link confidence threshold')
-    parser.add_argument('--cuda', default=torch.cuda.is_available(), type=bool, help='Use cuda for inference')
-    parser.add_argument('--canvas_size', default=1280, type=int, help='image size for inference')
-    parser.add_argument('--mag_ratio', default=1.5, type=float, help='image magnification ratio')
-    parser.add_argument('--poly', default=False, action='store_true', help='enable polygon type')
-    parser.add_argument('--show_time', default=False, action='store_true', help='show processing time')
     parser.add_argument('--test_folder', default='Data/test/test_images/', type=str, help='folder path to input images')
-    parser.add_argument('--refine', default=False, action='store_true', help='enable link refiner')
-    parser.add_argument('--refiner_model', default='weights/craft_refiner_CTW1500.pth', type=str, help='pretrained refiner model')
     args = parser.parse_args()
 
     result_folder = './optical_character_recognition/inference_examples/'
     if not os.path.isdir(result_folder):
         os.mkdir(result_folder)
-
-    t0 = time.time()
-    net = CRAFT()
-    if args.cuda:
-        net.load_state_dict(copyStateDict(torch.load(args.trained_model)))
-    else:
-        net.load_state_dict(copyStateDict(torch.load(args.trained_model, map_location='cpu')))
-    if args.cuda:
-        net = net.cuda()
-        net = torch.nn.DataParallel(net)
-        cudnn.benchmark = False
-    net.eval()
-
-    if args.refine:
-        from refinenet import RefineNet
-        refine_net = RefineNet()
-        print('Loading weights of refiner from checkpoint (' + args.refiner_model + ')')
-        if args.cuda:
-            refine_net.load_state_dict(copyStateDict(torch.load(args.refiner_model)))
-            refine_net = refine_net.cuda()
-            refine_net = torch.nn.DataParallel(refine_net)
-        else:
-            refine_net.load_state_dict(copyStateDict(torch.load(args.refiner_model, map_location='cpu')))
-        refine_net.eval()
-        args.poly = True
-    
     image_root = args.test_folder
     image_dir = os.listdir(image_root)
     image_path = os.path.join(image_root, random.choice(image_dir))
-    image = utils.loadImage(image_path)
-    bboxes, polys, score_text = text_detector(net, image, args.text_threshold, args.link_threshold, args.low_text, args.cuda, args.poly, args.canvas_size, args.mag_ratio, refine_net = None)
-    filename, file_ext = os.path.splitext(os.path.basename(image_path))
-    mask_file = result_folder + "/letter_heatmap_" + filename + '.jpg'
-    cv2.imwrite(mask_file, score_text)
-    utils.display(image_path, image[:,:,::-1], polys, dirname=result_folder)
+    
+    text_detector = TextDetection(args.trained_model).cuda()
+    boxes, polys, ret_score_text = text_detector(image_path)
+    breakpoint()
+    #text_detector.display(image_path, result_folder)
---
+++ optical_character_recognition/text_recognition.py
@@ -0,0 +1,35 @@
+import torch
+from optical_character_recognition import utils, models
+from torch.autograd import Variable
+from PIL import Image
+
+model_path = './optical_character_recognition/pretrained_models/crnn.pth'
+img_path = './Data/demos/demo_2.jpg'
+alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+model = models.CRNN(32, 1, 37, 256)
+if torch.cuda.is_available():
+    model = model.cuda()
+print('loading pretrained model from %s' % model_path)
+model.load_state_dict(torch.load(model_path))
+
+converter = utils.strLabelConverter(alphabet)
+
+transformer = utils.resizeNormalize((100, 32))
+image = Image.open(img_path).convert('L')
+image = transformer(image)
+if torch.cuda.is_available():
+    image = image.cuda()
+image = image.view(1, *image.size())
+image = Variable(image)
+
+model.eval()
+preds = model(image)
+
+_, preds = preds.max(2)
+preds = preds.transpose(1, 0).contiguous().view(-1)
+
+preds_size = Variable(torch.IntTensor([preds.size(0)]))
+raw_pred = converter.decode(preds.data, preds_size.data, raw=True)
+sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
+print('%-20s => %-20s' % (raw_pred, sim_pred))
